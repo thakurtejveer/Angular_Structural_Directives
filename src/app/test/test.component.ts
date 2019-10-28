157f57869a115cab848c21e706ce34b11ac89191
thakurtejveer <thakurtejveer1@gmail.com>
2019-10-28 15:37:41 +0530

    *ngIf is done using if and else ng-template

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,21 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-  // <h2 *ngIf="displayName; else elseBlock">
-  // Codevolution
-  // </h2>
-  // <ng-template #elseBlock>
-  //   <h2>
-  //   Name is hidden
-  //   </h2>
-  // </ng-template>
+
+  <div *ngIf="displayName; then thenBlock; else elseBlock"></div>
+  
+  <ng-template #thenBlock>
+    <h2>
+    Codevolution
+    </h2>
+  </ng-template>
+
+  <ng-template #elseBlock>
+    <h2>
+    Name is hidden
+    </h2>
+  </ng-template>
 
   `,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
 
-  public displayName=false;
+  public displayName=true;
 
   constructor() { }
 
